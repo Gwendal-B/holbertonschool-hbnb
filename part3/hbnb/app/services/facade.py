90e5f1310a677597504c8d360a0fe625b8f16602
Antgst <ant.gousset@gmail.com>
2026-03-11 10:05:24 +0100

    add: change the HBNBfacade to repository into db

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,4 +1,4 @@
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User
 from app.models.amenity import Amenity
 from app.models.place import Place
@@ -7,10 +7,15 @@ from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
+
+    def create_user(self, user_data):
+        user = User(**user_data)
+        self.user_repo.add(user)
+        return user
+
+    def get_user(self, user_id):
+        return self.user_repo.get(user_id)
 
     # ------------------------------------------------------------------ #
     #  Users
